553f49682e691b96a22130c09f655a26ee6c6a2b
Syed Wali Uddin Quadri <waliwasser29@gmail.com>
2022-12-17 23:16:45 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client'
 import {useEffect, useState} from 'react'
 import Loby from './Loby';
 import 'react-sound';
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://pixilit.onrender.com");
 
 
 function App() {
